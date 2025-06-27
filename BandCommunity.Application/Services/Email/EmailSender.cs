cddd7b9707f2746ebb894021ce5f442dd41e73ab
kleqing <nhanpk28122004@gmail.com>
2025-06-27 22:51:42 +0700

    [Add] Add email sender for forgot password, etc. (WIP)

---
+++ BandCommunity.Application/Services/Email/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Mail;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace BandCommunity.Application.Services.Email;
+
+public class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        try
+        {
+            string fromMail = "";
+            string fromPassword = "";
+            SmtpClient smtpClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com", // SMTP server
+                Port = 587, // TLS port
+                EnableSsl = true, // Enable TLS (SSL)
+                Credentials = new System.Net.NetworkCredential(fromMail, fromPassword) // My Username and password
+            };
+            MailMessage mail = new MailMessage();
+            mail.Subject = subject;
+            //mail.Body = GenerateEmailTemplate(email, subject, htmlMessage);
+
+            //Setting From , To and CC
+            mail.From = new MailAddress(fromMail);
+            mail.To.Add(new MailAddress(email));
+            mail.IsBodyHtml = true;
+            smtpClient.Send(mail);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return Task.CompletedTask;
+    }
+}
