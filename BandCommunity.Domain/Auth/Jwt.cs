ad782a0cb43dbe391461bf1f182db6b809c8d306
kleqing <nhanpk28122004@gmail.com>
2025-06-27 22:50:22 +0700

    [Add] Add Jwt auth token

---
+++ BandCommunity.Domain/Auth/Jwt.cs
@@ -0,0 +1,11 @@
+﻿namespace BandCommunity.Domain.Auth;
+
+public class Jwt
+{
+    public const string JWTOptionsKey = "JWT";
+	
+    public string Secret { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public int ExpiryInMinutes { get; set; }
+}
---
+++ BandCommunity.Infrastructure/Auth/IAuthTokenProcess.cs
@@ -0,0 +1,10 @@
+﻿using BandCommunity.Domain.Entities;
+
+namespace BandCommunity.Infrastructure.Auth;
+
+public interface IAuthTokenProcess
+{
+    (string Token, DateTime Expiry) GenerateToken(User user);
+    string GenerateRefreshToken();
+    void WriteAuthTokenAsHttpOnlyCookie(string cookieName, string token, DateTime expiry);
+}
