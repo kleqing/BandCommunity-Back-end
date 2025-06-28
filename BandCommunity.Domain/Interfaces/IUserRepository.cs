966891976cbb9b9692c1b0e1ec21ab7a812466d4
kleqing <nhanpk28122004@gmail.com>
2025-06-28 19:55:32 +0700

    [Add] Get user by get their refresh token

---
+++ BandCommunity.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using BandCommunity.Domain.Entities;
+
+namespace BandCommunity.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetUserByRefreshToken(string refreshToken);
+}
---
+++ BandCommunity.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using BandCommunity.Domain.Entities;
+using BandCommunity.Domain.Interfaces;
+using BandCommunity.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BandCommunity.Repository.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetUserByRefreshToken(string refreshToken)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.RefreshToken == refreshToken);
+        return user;
+    }
+}
