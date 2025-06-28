52c7d879532de21dec67fc1856b1a9cc690e6e04
kleqing <nhanpk28122004@gmail.com>
2025-06-28 13:40:23 +0700

    [Add] Add constant

---
+++ BandCommunity.Shared/Constant/LoginConstant.cs
@@ -0,0 +1,17 @@
+﻿namespace BandCommunity.Shared.Constant;
+
+public static class LoginConstant
+{
+    public static string LoginFailed = "Login failed. Please check your email and password.";
+    public static string AccountNotFound = "Account not found!";
+    public static string InvalidPassword = "Invalid password!";
+    public static string EmailNotConfirmed = "Email not confirmed!";
+    public static string EmailAlreadyConfirmed = "Email already confirmed!";
+    public static string ResendEmailSuccess = "Resend email successfully!";
+    public static string ResendEmailFailed = "Resend email failed!";
+    public static string ResetPasswordFailed = "Reset password failed!";
+    public static string CreateAccountFailed = "Create account failed! Please try again.";
+    public static string ClaimPrincipalFailed = "Claim principal failed!";
+    public static string RefreshTokenFailed = "Refresh token created failed! Please try again.";
+    public static string EmailExists = "Email already exists!";
+}
