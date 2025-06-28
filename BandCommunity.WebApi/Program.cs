77226b7665ca43ed6cfc838c8fd0130847b3b220
kleqing <nhanpk28122004@gmail.com>
2025-06-28 18:31:43 +0700

    [Update] Add google auth, redis database

--- BandCommunity.WebApi/Program.cs
+++ BandCommunity.WebApi/Program.cs
@@ -4,16 +4,19 @@ using BandCommunity.Domain.Entities;
 using BandCommunity.Domain.JWT;
 using BandCommunity.Infrastructure.Auth;
 using BandCommunity.Infrastructure.Data;
+using BandCommunity.Application.Services.Role;
 using dotenv.net;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using StackExchange.Redis;
 
 namespace BandCommunity.WebApi;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         DotEnv.Load();
 
@@ -57,9 +60,84 @@ public class Program
                 };
             });
 
+        var myAllowSpecificOrigins = "AllowAllOrigins";
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: myAllowSpecificOrigins,
+                policy =>
+                {
+                    policy.WithOrigins(Environment.GetEnvironmentVariable("FRONTEND_URL")!)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+        });
+        
+        //* Google Authentication
+        builder.Services.AddAuthentication()
+            .AddCookie()
+            .AddGoogle(options =>
+            {
+                var clientId = options.ClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID") ?? String.Empty;
+
+                if (string.IsNullOrEmpty(clientId))
+                {
+                    throw new Exception("Google Client ID is not set in environment variables.");
+                }
+                
+                var clientSecret = options.ClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET") ?? String.Empty;
+                
+                if (string.IsNullOrEmpty(clientSecret))
+                {
+                    throw new Exception("Google Client Secret is not set in environment variables.");
+                }
+                
+                options.ClientId = clientId;
+                options.ClientSecret = clientSecret;
+                options.ClaimActions.MapJsonKey("profile_picture", "profile_picture");
+                options.SaveTokens = true;
+                options.CallbackPath = "/signin-google";
+            });
+        
+        //* Redis Cache
+        builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            var redisConnectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
+    
+            if (string.IsNullOrWhiteSpace(redisConnectionString))
+            {
+                throw new InvalidOperationException("Redis connection string is not set in environment variables.");
+            }
+
+            var configurationOptions = ConfigurationOptions.Parse(redisConnectionString, true);
+            configurationOptions.AbortOnConnectFail = false;
+
+            try
+            {
+                return ConnectionMultiplexer.Connect(configurationOptions);
+            }
+            catch (RedisConnectionException ex)
+            {
+                throw new InvalidOperationException("Failed to connect to Redis: " + ex.Message, ex);
+            }
+        });
+
+        
+        builder.Services.AddScoped<IDatabase>(sp =>
+        {
+            var connectionMultiplexer = sp.GetRequiredService<IConnectionMultiplexer>();
+            return connectionMultiplexer.GetDatabase();
+        });
+        
+        //* Email Confirmation
+        builder.Services.Configure<IdentityOptions>(options =>
+        {
+            options.SignIn.RequireConfirmedEmail = true;
+        });
         
         //* Add services to the container.
         builder.Services.AddAuthorization();
+        builder.Services.AddHttpContextAccessor();
         builder.Services.AddControllers();
 
         //* Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -74,7 +152,15 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        
+        //* Seed role 
+        using (var scope = app.Services.CreateScope())
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+            await RoleServices.SeedRole(roleManager);
+        }
 
+        app.UseCors(myAllowSpecificOrigins);
         app.UseHttpsRedirection();
 
         app.UseAuthentication();
