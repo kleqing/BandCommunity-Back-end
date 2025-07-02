abe6e4a5a23abee9e167962ace4b38c0e05a39fd
kleqing <nhanpk28122004@gmail.com>
2025-07-02 20:53:08 +0700

    [Add] Add convert datetime to UTC (For PostgreSQL)

---
+++ BandCommunity.Shared/Helper/DateTimeExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace BandCommunity.Shared.Helper;
+
+public static class DateTimeExtension
+{
+    public static DateTime EnsureUtc(this DateTime dateTime)
+    {
+        //* Convert to UTC if not already in UTC
+        return dateTime.Kind switch
+        {
+            DateTimeKind.Unspecified => DateTime.SpecifyKind(dateTime, DateTimeKind.Utc),
+            DateTimeKind.Local => dateTime.ToUniversalTime(),
+            _ => dateTime, // Already in UTC
+        };
+    }
+}
